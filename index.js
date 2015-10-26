1814d7187eda8add74f86940830a443bb616763b
Matthias <matthias.dieudonne@gmail.com>
2015-10-26 16:21:51 +0100

    Ajout loader sc1 Suppresion loader routes inutiles

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 module.exports = {
     "routes": [
         {
-            "path" : "/exportCSV",
-            "value" : "exportCSV.js",
-            "method" : "get"
-        },
-        {
-            "path" : "/exportXML",
-            "value" : "exportXML.js",
-            "method" : "get"
-        },
-        {
-            "path" : "/exportZIP",
-            "value" : "exportZIP.js",
-            "method" : "get"
-        },
-        {
             "path" : "/search/:xmlid",
             "value" : "search.js",
             "method" : "get"
@@ -23,45 +8,10 @@ module.exports = {
     ],
     "loaders" : [
         {
-            "script" : "castor-load-xml",
-            "pattern" : "**/*.xml"
-        },
-        {
-            "script" : "castor-load-raw",
-            "pattern" : "**/*.xml"
-        },
-        {
             "script" : "tei-format/scenario1.js",
             "pattern" : "**/*.xml"
-        },
-        {
-            "script" : "tei-format/2014-11-01.js",
-            "pattern" : "**/*.xml"
-        },
-        {
-            "script" : "tei-format/2015-02-13.js",
-            "pattern" : "**/*.xml"
-        },
-        {
-            "script" : "autoScore.js",
-            "pattern" : "**/*.xml"
-        },
-        {
-            "script" : "castor-load-backup",
-            "pattern" : "**/*.xml",
-            "options" : {
-                "toKeep" : ["fields" , "keywords" , "progressNotedKeywords" , "progressSilenceKeywords" , "pertinenceMethods" , "timeJob" , "validatePertinence","validateSilence"]
-            }
-        },
-    ],
-    "flyingFields": {
-        "$listeDocuments": {
-            "mask": "_id,wid,basename,fields,progressSilenceKeywords,progressNotedKeywords,text"
-        },
-        "$document": {
-            "mask": "_id,wid,basename,fields,progressSilenceKeywords,progressNotedKeywords,validatePertinence,validateSilence,keywords,pertinenceMethods,text,timeJob"
         }
-    }
+    ]
 };
 
 module.exports.package = pkg = require('./package.json');
